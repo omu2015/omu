ff7c99a3b407167f2e39ebee40f9d880f103168c
woosung <woosung@ohda>
2015-09-07 21:08:09 +0900

    0907 0907분

--- src/main/java/com/bit2015/omu/service/TestService.java
+++ src/main/java/com/bit2015/omu/service/TestService.java
@@ -7,23 +7,74 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bit2015.omu.dao.MemberDao;
 import com.bit2015.omu.dao.ThemeDao;
+import com.bit2015.omu.vo.MemberVo;
 import com.bit2015.omu.vo.ThemeVo;
 
 @Service
 public class TestService {
 	
 	@Autowired
-	ThemeDao themeDao;
+	MemberDao memberDao;
 	
 	public void test(){
-		//List<BoardVo> list=boardDao.selectAll();
-		//List<BoardVo> list=ImgDao.selectAll();
+		
+		
+		//////////member.xml
+		
+		//List<MemberVo> list=memberDao.selectAll();
+		//System.out.println(list.toString());
+
+		//insert
+		/*
+		MemberVo memberVo=new MemberVo();
+		memberVo.setMemberId("woosungchu");
+		memberVo.setMemberName("주우성");
+		memberVo.setPassword("1234");
+		memberVo.setImageURL("abcd");
+		memberVo.setMemberGrade("guest");
+		memberVo.setMemberStatus("alive");
+		memberVo.setBirth("1992.10.10");
+		memberVo.setAddress("캉남큐");
+		memberVo.setPhoneNum1("010");
+		memberVo.setPhoneNum2("1234");
+		memberVo.setPhoneNum2("5678");
+		memberVo.setJoinDate("20150907");
+		memberVo.setJoinOutDate("hewillnot");
+		memberVo.setEmail("woosungchu@asad.com");
+		memberVo.setZipCode("137-140");
+		memberDao.insert(memberVo);
+		System.out.println(memberVo.toString());
+		List<MemberVo> list=memberDao.selectAll();
+		System.out.println(list.toString());
+		*/
+		
+		//delete
+		//memberDao.delete((long)8);
+		//List<MemberVo> list=memberDao.selectAll();
+		//System.out.println(list.toString());
+		
+		//selectVo
+		//MemberVo memberVo=memberDao.selectVo((long) 7);
+		//System.out.println(memberVo.toString());
+		
+		//update
+		List<MemberVo> list=memberDao.selectAll();
+		System.out.println(list.toString());
+		MemberVo memberVo=memberDao.selectVo((long) 7);
+		memberVo.setMemberName("기모다");
+		//memberDao.update(memberVo);
+		List<MemberVo> list2=memberDao.selectAll();
+		System.out.println(list2.toString());
+		
+		
+		//////////theme.xml
 
 		//insert
 		//ThemeVo themeVo=new ThemeVo();
-		//themeVo.setTheme_no(2);
-		//themeVo.setThemeName("피시방");
+		//themeVo.setTheme_no(1);
+		//themeVo.setThemeName("박진용");
 		//themeDao.insert(themeVo);
 		
 		//delete
@@ -43,7 +94,6 @@ public class TestService {
 		//	themeDao.update(themeVo);
 		//List<ThemeVo> list2=themeDao.selectAll();
 		//System.out.println(list2.toString());
-
 		
 		//System.out.println(list.toString());
 		
