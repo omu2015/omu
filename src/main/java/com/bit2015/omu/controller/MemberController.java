f8fc9c7a197b369f76eed2716553c913becd7527
BIT-PC <BIT-PC@BIT>
2015-09-07 10:46:22 +0900

    1st chunjinhan

---
+++ src/main/java/com/bit2015/omu/controller/MemberController.java
@@ -0,0 +1,8 @@
+package com.bit2015.omu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
---
+++ src/main/java/com/bit2015/omu/controller/ReviewController.java
@@ -0,0 +1,7 @@
+package com.bit2015.omu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ReviewController {
+}
