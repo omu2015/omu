49563803cb5aa2043e2c26c55c60479a427ddb58
woosung <woosung@ohda>
2015-09-07 13:46:00 +0900

    진한이형 pull해요

--- src/main/java/com/bit2015/omu/dao/MemberDao.java
+++ src/main/java/com/bit2015/omu/dao/MemberDao.java
@@ -1,5 +1,16 @@
 package com.bit2015.omu.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDao {
 
+	@Autowired
+	SqlMapClientTemplate sqlMapClientTemplate;
+	
+	public void selectAll(){
+		
+	}
 }
