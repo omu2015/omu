bec19698d4bb1c57d22f25969fd5a4f7dce10c24
woosung <woosung@ohda>
2015-09-07 17:00:31 +0900

    themeTest

---
+++ src/main/java/com/bit2015/omu/dao/ThemeDao.java
@@ -0,0 +1,32 @@
+package com.bit2015.omu.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.bit2015.omu.vo.ImgVo;
+import com.bit2015.omu.vo.MemberVo;
+import com.bit2015.omu.vo.ThemeVo;
+
+@Repository
+public class ThemeDao {
+
+	@Autowired
+	SqlMapClientTemplate sqlMapClientTemplate;
+	
+	public List<ThemeVo> selectAll(){
+		List<ThemeVo> list= sqlMapClientTemplate.queryForList("theme.selectAll");
+		return list;
+	}
+	
+	public void insert(ThemeVo themeVo){
+		sqlMapClientTemplate.insert("member.insert", themeVo);
+	}
+	
+	
+	
+	
+	
+}
--- src/main/java/com/bit2015/omu/service/TestService.java
+++ src/main/java/com/bit2015/omu/service/TestService.java
@@ -1,25 +1,34 @@
 package com.bit2015.omu.service;
 
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bit2015.omu.dao.BoardDao;
-import com.bit2015.omu.dao.ImgDao;
-import com.bit2015.omu.dao.MemberDao;
-import com.bit2015.omu.vo.BoardVo;
-import com.bit2015.omu.vo.MemberVo;
+import com.bit2015.omu.dao.ThemeDao;
+import com.bit2015.omu.vo.ThemeVo;
 
 @Service
 public class TestService {
 	
 	@Autowired
-	BoardDao boardDao;
+	ThemeDao themeDao;
 	
 	public void test(){
 		//List<BoardVo> list=boardDao.selectAll();
-		List<BoardVo> list=ImgDao.selectAll();
-		System.out.println(list.toString());
+		//List<BoardVo> list=ImgDao.selectAll();
+		ThemeVo themeVo=new ThemeVo();
+		themeVo.setTheme_no(1);
+		themeVo.setThemeName("노래방");
+		
+		themeDao.insert(themeVo);
+		List<ThemeVo> list=themeDao.selectAll();
+		System.out.println(list.get(0).getTheme_no()+list.get(0).getThemeName());
+		
+		//System.out.println(list.toString());
+		
 	}
 }
