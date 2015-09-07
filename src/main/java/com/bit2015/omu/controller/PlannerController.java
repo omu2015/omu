612442f911c0bd54d75abb08e89b73409da767a6
pjy <pjy@pjy-PC>
2015-09-07 21:08:59 +0900

    2015/09/07 오후9시

--- src/main/java/com/bit2015/omu/controller/PlannerController.java
+++ src/main/java/com/bit2015/omu/controller/PlannerController.java
@@ -1,7 +1,23 @@
 package com.bit2015.omu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bit2015.omu.service.PJYTestService2;
 
 @Controller
+@RequestMapping("/pjy")
 public class PlannerController {
+	
+	
+
+		@Autowired
+		PJYTestService2 testService2;
+
+
+		@RequestMapping("/test")
+		public void test(){
+			testService2.test();
+		}	
 }
