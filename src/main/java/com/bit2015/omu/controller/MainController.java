0adc60661ed2b328132101332022e8ede085678d
성국 <@psk>
2015-09-07 13:21:43 +0900

    start

--- src/main/java/com/bit2015/omu/controller/MainController.java
+++ src/main/java/com/bit2015/omu/controller/MainController.java
@@ -2,7 +2,12 @@ package com.bit2015.omu.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	@RequestMapping("/")
+	public String index(){
+		return "main/index";
+	}	
 }
